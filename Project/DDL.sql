0f17d0fe02c9fead3a635a445856ef08738f93f0
tokenasian <78766033+tokenasian@users.noreply.github.com>
2022-10-17 18:16:03 -0400

    Update DDL.sql

--- Project/DDL.sql
+++ Project/DDL.sql
@@ -1,6 +1,13 @@
-SET FOREIGN_KEY_CHECKS=0;
+SET FOREIGN_KEY_CHECKS = 0;
 SET AUTOCOMMIT = 0;
 
+DROP TABLE IF EXISTS Passengers;
+DROP TABLE IF EXISTS Flights;
+DROP TABLE IF EXISTS Airports;
+DROP TABLE IF EXISTS Tickets;
+DROP TABLE IF EXISTS Ticket_Classes;
+
+/* Create Passengers Table*/
 CREATE OR REPLACE TABLE Passengers(
     passenger_id int AUTO_INCREMENT NOT NULL,
     first_name varchar(255) NOT NULL,
@@ -12,6 +19,7 @@ CREATE OR REPLACE TABLE Passengers(
     PRIMARY KEY (passenger_id)
 );
 
+/* Create Flights Table*/
 CREATE OR REPLACE TABLE Flights(
     flight_id int AUTO_INCREMENT NOT NULL,
     departing_airport int NOT NULL,
@@ -25,6 +33,7 @@ CREATE OR REPLACE TABLE Flights(
     FOREIGN KEY (arrival_airport) REFERENCES Airports(airport_id)
 );
 
+/* Create Airports Table*/
 CREATE OR REPLACE TABLE Airports(
     airport_id int AUTO_INCREMENT NOT NULL,
     airport_name varchar(255) NOT NULL,
@@ -33,44 +42,71 @@ CREATE OR REPLACE TABLE Airports(
     PRIMARY KEY (airport_id)
 );
 
+/* Create Tickets Table*/
 CREATE OR REPLACE TABLE Tickets(
     ticket_id int AUTO_INCREMENT NOT NULL,
     passenger_id int NOT NULL,
     flight_id int NOT NULL,
-    ticket_class varchar(10) NOT NULL,
+    ticket_class int NOT NULL,
     PRIMARY KEY (ticket_id), 
     FOREIGN KEY (passenger_id) REFERENCES Passengers(passenger_id),
     FOREIGN KEY (flight_id) REFERENCES Flights(flight_id),
     FOREIGN KEY (ticket_class) REFERENCES Ticket_Classes(class_id)
 );
 
+/* Create Ticket_Classes Table*/
 CREATE OR REPLACE TABLE Ticket_Classes(
     class_id int AUTO_INCREMENT NOT NULL,
-    class_name varchar(10) NOT NULL,
+    class_name varchar(25) NOT NULL,
     upgrade_charge int NOT NULL,
     PRIMARY KEY (class_id)
 );
 
+/* Create Intersections*/
+
 INSERT INTO Passengers (first_name, last_name, passport, email, phone_number)
-VALUES('Sterling', 'Archer', '652190223', 'archer@hello.com', '814-825-5951'),
-('Stede', 'Bonnet', '141011810', 'bonnet@hello.com', '919-252-6000'),
-('Michael', 'Scott', '498726075', 'scott@hello.com', '716-475-1975'),
+VALUES('Sterling', 'Archer', '542637785', 'archer@hello.com', '814-825-5951'),
+('Stede', 'Bonnet', '919608451', 'bonnet@hello.com', '919-252-6000'),
+('Steve', 'Harrington', '135516591', 'harrington@hello.com', '505-820-2961'),
 ('Mabel', 'Mora', '637071702', 'mora@hello.com', '315-794-6533'),
-('Jeff', 'Winger', '576803679', 'winger@hello.com', '408-558-2426');
+('Michael', 'Scott', '571921982', 'scott@hello.com', '716-475-1975'),
+('Buffy', 'Summers', '678996728', 'summers@hello.com', '813-273-1085'),
+('Jeff', 'Winger', '218571886', 'winger@hello.com', '408-558-2426');
 
 INSERT INTO Airports (airport_name, airport_location)
 VALUES ('LaGuardia Airport', 'New York'),
-('Incheon Airport', 'Seoul'),
+('Sydney Airport', 'Australia'),
+('Dublin Airport', 'Ireland'),
 ('Malpensa Airport', 'Milan'),
-('Narita Airport', 'Tokyo'),
-('Seville Airport', 'Spain');
+('Incheon Airport', 'Seoul'),
+('Seville Airport', 'Spain'),
+('Narita Airport', 'Tokyo');
 
 INSERT INTO Ticket_Classes(class_name, upgrade_charge)
 VALUES
-('First', '5000'),
-('Business', '1000'), 
-('Premium Economy', '500'),
-('Economy', '0');
+('First Class', 5000),
+('Business', 1000), 
+('Premium Economy', 500),
+('Economy', 0);
 
-SET FOREIGN_KEY_CHECKS=1;
+INSERT INTO Flights (departing_airport, arrival_airport, departure_time, arrival_time, air_fare, capacity)
+VALUES
+(
+    (SELECT Airports.airport_id FROM Airports WHERE Airports.airport_name = 'LaGuardia Airport'),
+    (SELECT Airports.airport_id FROM Airports WHERE Airports.airport_name = 'Sydney Airport'),
+    '2022-10-30 08:00:00',
+    '2022-10-30 14:00:00',
+    5000,
+    70
+);
+
+INSERT INTO Tickets (passenger_id, flight_id, ticket_id)
+VALUES
+(
+    (SELECT Passengers.passenger_id FROM Passengers WHERE Passengers.passport = '919608451'),
+    (SELECT Flights.flight_id FROM Flights WHERE Flights.flight_id = 1),
+    (SELECT Ticket_Classes.class_id FROM Ticket_Classes WHERE Ticket_Classes.class_name = 'First Class')
+);
+
+SET FOREIGN_KEY_CHECKS = 1;
 SET AUTOCOMMIT = 1;
