3daf9e2983348559a73600dcb797ebd90c1b841b
tokenasian <78766033+tokenasian@users.noreply.github.com>
2022-11-01 06:55:21 -0400

    Update DDL.sql

--- Project/DDL.sql
+++ Project/DDL.sql
@@ -6,9 +6,13 @@ DROP TABLE IF EXISTS Flights;
 DROP TABLE IF EXISTS Airports;
 DROP TABLE IF EXISTS Tickets;
 DROP TABLE IF EXISTS Ticket_Classes;
+DROP TABLE IF EXISTS Itineraries;
 
-/* Create Passengers Table*/
-CREATE OR REPLACE TABLE Passengers(
+
+-- -----------------------------------------------------
+-- Passengers Table
+-- -----------------------------------------------------
+CREATE TABLE Passengers(
     passenger_id int AUTO_INCREMENT NOT NULL,
     first_name varchar(255) NOT NULL,
     last_name varchar(255) NOT NULL,
@@ -19,22 +23,26 @@ CREATE OR REPLACE TABLE Passengers(
     PRIMARY KEY (passenger_id)
 );
 
-/* Create Flights Table*/
-CREATE OR REPLACE TABLE Flights(
+-- -----------------------------------------------------
+-- Flights Table 
+-- -----------------------------------------------------
+CREATE TABLE Flights(
     flight_id int AUTO_INCREMENT NOT NULL,
-    departing_airport int NOT NULL,
+    departure_airport int NOT NULL,
     arrival_airport int NOT NULL,
     departure_time timestamp NOT NULL,
     arrival_time timestamp NOT NULL,
     air_fare int NOT NULL,
     capacity int NOT NULL,
     PRIMARY KEY (flight_id),
-    FOREIGN KEY (departing_airport) REFERENCES Airports(airport_id),
-    FOREIGN KEY (arrival_airport) REFERENCES Airports(airport_id)
+    FOREIGN KEY (departure_airport) REFERENCES Airports(airport_id) ON DELETE CASCADE,
+    FOREIGN KEY (arrival_airport) REFERENCES Airports(airport_id) ON DELETE CASCADE
 );
 
-/* Create Airports Table*/
-CREATE OR REPLACE TABLE Airports(
+-- -----------------------------------------------------
+-- Airports Table 
+-- -----------------------------------------------------
+CREATE TABLE Airports(
     airport_id int AUTO_INCREMENT NOT NULL,
     airport_name varchar(255) NOT NULL,
     airport_location varchar(255) NOT NULL,
@@ -42,28 +50,45 @@ CREATE OR REPLACE TABLE Airports(
     PRIMARY KEY (airport_id)
 );
 
-/* Create Tickets Table*/
-CREATE OR REPLACE TABLE Tickets(
+-- -----------------------------------------------------
+-- Tickets Table 
+-- -----------------------------------------------------
+CREATE TABLE Tickets(
     ticket_id int AUTO_INCREMENT NOT NULL,
-    passenger_id int NOT NULL,
+    itinerary_id int NOT NULL,
     flight_id int NOT NULL,
     ticket_class int NOT NULL,
     PRIMARY KEY (ticket_id), 
-    FOREIGN KEY (passenger_id) REFERENCES Passengers(passenger_id),
-    FOREIGN KEY (flight_id) REFERENCES Flights(flight_id),
+    FOREIGN KEY (itinerary_id) REFERENCES Itineraries(itinerary_id) ON DELETE CASCADE,
+    FOREIGN KEY (flight_id) REFERENCES Flights(flight_id) ON DELETE CASCADE,
     FOREIGN KEY (ticket_class) REFERENCES Ticket_Classes(class_id)
 );
 
-/* Create Ticket_Classes Table*/
-CREATE OR REPLACE TABLE Ticket_Classes(
+-- -----------------------------------------------------
+-- Ticket_Classes Table 
+-- -----------------------------------------------------
+CREATE TABLE Ticket_Classes(
     class_id int AUTO_INCREMENT NOT NULL,
     class_name varchar(25) NOT NULL,
     upgrade_charge int NOT NULL,
     PRIMARY KEY (class_id)
 );
 
-/* Create Intersections*/
+-- -----------------------------------------------------
+-- Itineraries Table 
+-- -----------------------------------------------------
+CREATE TABLE Itineraries(
+    itinerary_id int AUTO_INCREMENT NOT NULL,
+    passenger_id int NOT NULL,
+    trip_name varchar(255) UNIQUE NOT NULL,
+    PRIMARY KEY (itinerary_id),
+    FOREIGN KEY (passenger_id) REFERENCES Passengers(passenger_id) ON DELETE CASCADE
+);
 
+
+-- -----------------------------------------------------
+-- Sample Data  
+-- -----------------------------------------------------
 INSERT INTO Passengers (first_name, last_name, passport, email, phone_number)
 VALUES('Sterling', 'Archer', '542637785', 'archer@hello.com', '814-825-5951'),
 ('Stede', 'Bonnet', '919608451', 'bonnet@hello.com', '919-252-6000'),
@@ -89,7 +114,7 @@ VALUES
 ('Premium Economy', 500),
 ('Economy', 0);
 
-INSERT INTO Flights (departing_airport, arrival_airport, departure_time, arrival_time, air_fare, capacity)
+INSERT INTO Flights (departure_airport, arrival_airport, departure_time, arrival_time, air_fare, capacity)
 VALUES
 (
     (SELECT Airports.airport_id FROM Airports WHERE Airports.airport_name = 'LaGuardia Airport'),
@@ -98,15 +123,64 @@ VALUES
     '2022-10-30 14:00:00',
     5000,
     70
+),
+(
+    (SELECT Airports.airport_id FROM Airports WHERE Airports.airport_name = 'Sydney Airport'),
+    (SELECT Airports.airport_id FROM Airports WHERE Airports.airport_name = 'Incheon Airport'),
+    '2022-11-01 07:00:00',
+    '2022-11-01 15:00:00',
+    5000,
+    70
+),
+(
+    (SELECT Airports.airport_id FROM Airports WHERE Airports.airport_name = 'LaGuardia Airport'),
+    (SELECT Airports.airport_id FROM Airports WHERE Airports.airport_name = 'Narita Airport'),
+    '2023-02-20 08:00:00',
+    '2023-02-21 20:00:00',
+    5000,
+    70
 );
 
-INSERT INTO Tickets (passenger_id, flight_id, ticket_id)
+
+INSERT INTO Itineraries (passenger_id, trip_name)
 VALUES
 (
+    (SELECT Passengers.passenger_id FROM Passengers WHERE Passengers.passport = '542637785'),
+    "Archer Vacation 2022"
+),
+(
     (SELECT Passengers.passenger_id FROM Passengers WHERE Passengers.passport = '919608451'),
-    (SELECT Flights.flight_id FROM Flights WHERE Flights.flight_id = 1),
-    (SELECT Ticket_Classes.class_id FROM Ticket_Classes WHERE Ticket_Classes.class_name = 'First Class')
+    "Bonnet Business Trip"
+),
+(
+    (SELECT Passengers.passenger_id FROM Passengers WHERE Passengers.passport = '678996728'),
+    "Buffy Family Trip"
 );
 
-SET FOREIGN_KEY_CHECKS = 1;
+
+INSERT INTO Tickets (itinerary_id, flight_id, ticket_class)
+VALUES
+(
+    (SELECT Itineraries.itinerary_id FROM Itineraries WHERE Itineraries.trip_name = 'Archer Vacation 2022'),
+    (SELECT Flights.flight_id FROM Flights WHERE Flights.flight_id = 1),
+    (SELECT Ticket_Classes.class_id FROM Ticket_Classes WHERE Ticket_Classes.class_name = 'First Class')
+),
+(
+    (SELECT Itineraries.itinerary_id FROM Itineraries WHERE Itineraries.trip_name = 'Bonnet Business Trip'),
+    (SELECT Flights.flight_id FROM Flights WHERE Flights.flight_id = 1),
+    (SELECT Ticket_Classes.class_id FROM Ticket_Classes WHERE Ticket_Classes.class_name = 'Business')
+),
+(
+    (SELECT Itineraries.itinerary_id FROM Itineraries WHERE Itineraries.trip_name = 'Bonnet Business Trip'),
+    (SELECT Flights.flight_id FROM Flights WHERE Flights.flight_id = 2),
+    (SELECT Ticket_Classes.class_id FROM Ticket_Classes WHERE Ticket_Classes.class_name = 'Business')
+),
+(
+    (SELECT Itineraries.itinerary_id FROM Itineraries WHERE Itineraries.trip_name = 'Buffy Family Trip'),
+    (SELECT Flights.flight_id FROM Flights WHERE Flights.flight_id = 3),
+    (SELECT Ticket_Classes.class_id FROM Ticket_Classes WHERE Ticket_Classes.class_name = 'Economy')
+);
+
+
+SET FOREIGN_KEY_CHECKS=1;
 SET AUTOCOMMIT = 1;
